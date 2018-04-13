bb2a64bde837eed67329cf41fa546823dd45c6e4
RichardLeben <shiro_tan_kuro_tan@yahoo.co.jp>
2018-04-14 02:07:48 +0900

    support different table

--- UMA.js
+++ UMA.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           UMA
 // @namespace      http://tampermonkey.net/
-// @version        0.2
+// @version        0.3
 // @description    try to take over the world!
 // @author         You
 // @match          http://jra.jp/JRADB/*
@@ -11,15 +11,20 @@
 // ==/UserScript==
 
 (function($) {
-    var umamei_list = jQuery(".umameiCol").map(function(){ return this.innerText;});
+  var $uma_tr = $(".mainList tr").filter(function(){ return $(this).children("td.umaKihon").length > 0; });
+  var umamei_list = $uma_tr.find(".umameiCol").map(function(){ return this.innerText;});
+  if (umamei_list.length == 0) {
+    umamei_list =  $uma_tr.find(".umamei").map(function(){ return this.innerText;});
     if (umamei_list.length == 0) {
-        umamei_list = jQuery(".umamei").map(function(){ return this.innerText;});
+      $uma_tr = $(".mainList tr").filter(function(){ return $(this).children("td.umameiCol").length > 0; });
+      umamei_list =  $uma_tr.find(".mokuyouUmamei").map(function(){ return this.innerText;});
     }
-    if (umamei_list.length > 0) {
-        $('body').append('<div class="umapanel parent" style="height:200px; width:1000px; position:absolute; top:200px; left:200px; border:1px solid #000000; background-color: #ffffff">馬パネル<div>');
-        umamei_list.each(function(index){
-            $('.parent').append('<div class="umapanel" style="height:50px; width:50px; border:1px solid #000000; top:-'+ index*52 +'px; left:'+ index*52 +'px;">'+ this +'<div>');
-        });
-        $('.umapanel').draggable();
-    }
+  }
+  if (umamei_list.length > 0) {
+    $('body').append('<div class="umapanel parent" style="height:200px; width:1000px; position:absolute; top:200px; left:200px; border:1px solid #000000; background-color: #ffffff">馬パネル<div>');
+    umamei_list.each(function(index){
+        $('.parent').append('<div class="umapanel" style="height:50px; width:50px; border:1px solid #000000; top:-'+ index*52 +'px; left:'+ index*52 +'px;">'+ this +'<div>');
+    });
+    $('.umapanel').draggable();
+  }
 })(jQuery);
