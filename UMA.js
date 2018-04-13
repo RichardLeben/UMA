3770d901b86622d531cf2ac977d6d1599456fc69
RichardLeben <shiro_tan_kuro_tan@yahoo.co.jp>
2018-04-14 00:16:07 +0900

    improve panel display

--- UMA.js
+++ UMA.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           UMA
 // @namespace      http://tampermonkey.net/
-// @version        0.1
+// @version        0.2
 // @description    try to take over the world!
 // @author         You
 // @match          http://jra.jp/JRADB/*
@@ -13,11 +13,13 @@
 (function($) {
     var umamei_list = jQuery(".umameiCol").map(function(){ return this.innerText;});
     if (umamei_list.length == 0) {
-        umamei_list = jQuery(".umameiCol").map(function(){ return this.innerText;});
+        umamei_list = jQuery(".umamei").map(function(){ return this.innerText;});
     }
-    $('body').append('<div class="umapanel parent" style="height:200px; width:1000px; position:absolute; top:200px; left:200px; border:1px solid #000000; background-color: #ffffff">馬パネル<div>');
-    umamei_list.each(function(index){
-        $('.parent').append('<div class="umapanel" style="height:50px; width:50px; border:1px solid #000000; top:-'+ index*52 +'px; left:'+ index*52 +'px;">'+ this +'<div>');
-    });
-    $('.umapanel').draggable();
+    if (umamei_list.length > 0) {
+        $('body').append('<div class="umapanel parent" style="height:200px; width:1000px; position:absolute; top:200px; left:200px; border:1px solid #000000; background-color: #ffffff">馬パネル<div>');
+        umamei_list.each(function(index){
+            $('.parent').append('<div class="umapanel" style="height:50px; width:50px; border:1px solid #000000; top:-'+ index*52 +'px; left:'+ index*52 +'px;">'+ this +'<div>');
+        });
+        $('.umapanel').draggable();
+    }
 })(jQuery);
